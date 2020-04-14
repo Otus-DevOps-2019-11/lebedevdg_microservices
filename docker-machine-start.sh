5a378a33d94d6bd6acb179f17e3071903a29f8ed
Denis Lebedev <lebedevdg@ekassir.com>
2020-04-14 22:56:09 +0300

    monitoring-2_3

--- docker-machine-start.sh
+++ docker-machine-start.sh
@@ -8,6 +8,8 @@ name=${1:-docker-host}
 echo $name
 
 docker-machine create --driver google --google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts \
---google-machine-type n1-standard-1 --google-project docker3-273507 --google-zone europe-north1-b --google-open-port 9292/tcp --google-open-port 9090/tcp $name
+--google-machine-type n1-standard-1 --google-project docker3-273507 --google-zone europe-north1-b --google-open-port 9292/tcp --google-open-port 9090/tcp --google-open-port 8080/tcp --google-open-port 3000/tcp $name
 
 eval $(docker-machine env $name)
+
+##eval "$(docker-machine env -u)"
